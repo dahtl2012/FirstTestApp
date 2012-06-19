591c6e4d0bd2a8333902740ce99e2022171ae1c8
Dominik Arnhof <d.arnhof@gmail.com>
2012-06-19 14:12:03 +0200

    app

---
+++ FirstTestApp/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  FirstTestApp
+//
+//  Created by Dominik Arnhof on 19.06.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
