819831bd23ed030fb4118c04735b9a4c2b2f3056
Klaus Bauernfeind <bauernfeind.klaus@gmx.at>
2012-09-12 21:54:39 +0200

    Implementierung der Wasserwaage

--- FirstTestApp/FirstViewController.h
+++ FirstTestApp/FirstViewController.h
@@ -7,7 +7,16 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreMotion/CoreMotion.h>
 
 @interface FirstViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UIView *level;
+@property (strong, nonatomic) IBOutlet UILabel *angel;
+
+@property (strong) CMMotionManager *motionManager;
+@property (assign) double x;
+@property (assign) double y;
+@property (assign) double z;
 
 @end
